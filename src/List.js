3715e553001401eca8b09b764afcaa48e3817e81
TudorDan <tudor.ist@gmail.com>
2018-08-28 23:56:31 +0300

    feat: Style list scrollbar

--- src/List.js
+++ src/List.js
@@ -16,17 +16,17 @@ class List extends Component{
 		const { venues , selected } = this.props
 		const { visible } = this.state
 		return (
-			<div className="list-venues">
-				<h2>Top venues</h2>
-				<ul className="venues-list">
-					{venues.map(venue => { return (
-						<li key={venue.id}>
-							<h3>{venue.name}</h3>
-							<p>{venue.type}</p>
-						</li>
-					)})}
-				</ul>
-			</div>
+				<div className="list-venues">
+					<h2>Top venues</h2>
+					<ul className="venues-list">
+						{venues.map(venue => { return (
+							<li key={venue.id}>
+								<h3>{venue.name}</h3>
+								<p>{venue.type}</p>
+							</li>
+						)})}
+					</ul>
+				</div>
 		)
 	}
 }
