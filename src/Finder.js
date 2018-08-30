4bcda0ce23b82159d7a8f2148eb2324c645fee5a
TudorDan <tudor.ist@gmail.com>
2018-08-30 20:33:05 +0300

    fix: Handle Google Maps API authentication errors

--- src/Finder.js
+++ src/Finder.js
@@ -62,10 +62,10 @@ class Finder extends Component {
 					selected={selectedID}
 					center={center}
 					onSelection={this.updateSelected}
-					googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBT-eboO6ZtfUG6q-eTsNw3VM3pZvoQi6g&v=3.exp"
-					loadingElement={<div style={{ height: `100vh` }} />}
-					containerElement={<div style={{ height: `100vh` }} />}
-					mapElement={<div style={{ height: `100vh` }} />}
+					googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyBT-eboO6ZtfUG6q-eTsNw3VM3pZvoQi6a&v=3.exp"
+					loadingElement={<div style={{ height: `100vh` }} id="mapLoading"/>}
+					containerElement={<div style={{ height: `100vh` }} id="mapContainer" />}
+					mapElement={<div style={{ height: `100vh` }} id="mapElement"/>}
 				/>
 			</div>
 		)
--- src/MapComp.js
+++ src/MapComp.js
@@ -2,27 +2,35 @@ import React from 'react'
 import { withScriptjs, withGoogleMap, GoogleMap, Marker , InfoWindow } from 'react-google-maps'
 import PropTypes from 'prop-types'
 
+window.gm_authFailure = () => {
+	document.getElementById('mapElement').innerHTML = 
+		`<div class="error" style="padding-left:256px;"> 
+			Unable to authenticate Google Maps API access. Please check  
+			<a href="https://kb.mailster.co/how-can-i-open-the-browsers-console/">the console</a> and contact 
+			<a href="https://github.com/TudorDan">the app admin</a></div>`
+}
+
 const MapComp = withScriptjs(withGoogleMap((props) => {
 	const { venues , center , selected , onSelection } = props
 	return <GoogleMap
-		defaultCenter={ center }
-		defaultZoom={14}
-		>
-			{venues.map(venue => <Marker
-				key={ venue.id }
-				position={ venue.location }
-				animation={ selected===venue.id ? window.google.maps.Animation.BOUNCE : window.google.maps.Animation.DROP }
-				onClick={ () => onSelection(venue.id) }
-			>	{selected===venue.id 
-				? <InfoWindow>
-						<div className="infodiv">
-							<h3>{venue.name}</h3>
-							<p><strong>Adress:</strong> {venue.adress} </p>
-						</div>
-					</InfoWindow>
-				: ''}
-			</Marker>)}
-	</GoogleMap>
+			defaultCenter={ center }
+			defaultZoom={14}
+			>
+				{venues.map(venue => <Marker
+					key={ venue.id }
+					position={ venue.location }
+					animation={ selected===venue.id ? window.google.maps.Animation.BOUNCE : window.google.maps.Animation.DROP }
+					onClick={ () => onSelection(venue.id) }
+				>	{selected===venue.id 
+					? <InfoWindow>
+							<div className="infodiv">
+								<h3>{venue.name}</h3>
+								<p><strong>Adress:</strong> {venue.adress} </p>
+							</div>
+						</InfoWindow>
+					: ''}
+				</Marker>)}
+		</GoogleMap>		
 }))
 
 MapComp.PropTypes = {
